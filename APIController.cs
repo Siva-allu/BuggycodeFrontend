302d57a25c3c418512ac9cf88154cb42e65cbd95
Siva-allu <101029742+Siva-allu@users.noreply.github.com>
2025-09-22 15:04:29 +0530

    Add files via upload

---
+++ APIController.cs
@@ -0,0 +1,23 @@
+
+using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlClient;
+
+
+namespace BuggyApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class DataController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetData()
+        {
+            string result = null;
+            if (result.Length > 0) // will throw NullReferenceException
+            {
+                return Ok(new { message = "Data fetched" });
+            }
+            return BadRequest("No data");
+        }
+    }
+}
